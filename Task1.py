462777a9d107b4db5ad541513889162a25e712ac
Kunal Mendhe <100359410+Kunal-Mendhe@users.noreply.github.com>
2025-07-02 14:38:09 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a=input('Enter The First Number:')
+a=int(a)
+b=input('Enter The Second Number:')
+b=int(b)
+c= a+b
+d= a-b
+e= a*b
+f= a/b
+print('Addition:',c)
+print('Subtraction:',d)
+print('Multiplication:',e)
+print('Division:',f)
